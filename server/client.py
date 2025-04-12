84c6b3c9a1034466c14123edf44946e96ff3b623
Colton Mazur <crm97068@uga.edu>
2025-04-12 12:55:21 -0400

    updating files

--- server/client.py
+++ server/client.py
@@ -7,6 +7,19 @@ import socket
 # may or may not be established)
 #----------------------------------------------------------
 def __main__(): #main function
-    print(1)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 7000)) #change later to connect to front end
+    client_socket.sendall(b"Hello from client!")  # Send message
+    data = client_socket.recv(1024)  # Receive response
+    print(f"Recieved from server: {data.decode()}")
+    alive = True
+    while alive:
+        message = input()
+        send = bytes(message)
+        client_socket.sendall(f"{send}")
+        if message == 'exit':
+            alive = False
+    client_socket.close()
+    print("Client closed")
 if __name__ == '__main__':
     __main__()
--- server/emailServer.py
+++ server/emailServer.py
@@ -1,6 +1,27 @@
 #emailServer
-
+import socket
+from multiprocessing import Process
+import os
 #--------------------------------------------------------------------
 # This file holds the emailServer interface that holds email
 # clients and establishes communication lines between clients
 # SQLite database implemented to handle accounts
+#
+# (This will likely get changed to the front end server functionality later)
+# (This will mainly be used to test the client end)
+#--------------------------------------------------------------------
+def __main__():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind('localhost', 7000) #change to handle multiple client connections using multi-processesing
+    server_socket.listen(1)
+    print("Server listening on port 7000")
+    connection, addr = server_socket.accept()
+    print(f"Connected to {addr}")
+    data = server_socket.recv(1024)
+    print(f"Recieved from client {data.decode()}")
+    connection.sendall(b"Enter username and password or exit")
+    if (data.decode() == 'exit'):
+        server_socket.close()
+    print("Closed")
+if __name__ == '__main__':
+    __main__()
