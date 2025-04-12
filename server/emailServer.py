2a78de062da918bdd8dfd8b9401bfdaa0adebcd0
Colton Mazur <crm97068@uga.edu>
2025-04-12 13:24:53 -0400

    update

--- server/emailServer.py
+++ server/emailServer.py
@@ -12,16 +12,19 @@ import os
 #--------------------------------------------------------------------
 def __main__():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind('localhost', 7000) #change to handle multiple client connections using multi-processesing
-    server_socket.listen(1)
-    print("Server listening on port 7000")
-    connection, addr = server_socket.accept()
-    print(f"Connected to {addr}")
-    data = server_socket.recv(1024)
-    print(f"Recieved from client {data.decode()}")
-    connection.sendall(b"Enter username and password or exit")
-    if (data.decode() == 'exit'):
-        server_socket.close()
-    print("Closed")
+    server_socket.bind(('localhost', 7000)) #change to handle multiple client connections using multi-processesing
+    alive = True
+    while alive:
+        server_socket.listen(1)
+        print("Server listening on port 7000")
+        connection, addr = server_socket.accept()
+        print(f"Connected to {addr}")
+        data = server_socket.recv(1024)
+        print(f"Recieved from client {data.decode()}")
+        connection.sendall(b"Enter username and password or exit")
+        if (data.decode() == 'exit'):
+            server_socket.close()
+            alive = False
+    print("Closed Server")
 if __name__ == '__main__':
     __main__()
