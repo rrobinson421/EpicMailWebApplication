ee4a58f8386313d829d6a8846279af8db115ca2f
rrobinson421 <123380520+rrobinson421@users.noreply.github.com>
2025-05-04 14:30:20 -0400

    Implemented Email Deletion

--- src/components/EmailManagement.tsx
+++ src/components/EmailManagement.tsx
@@ -231,7 +231,6 @@ const EmailManagement: React.FC = () => {
           <EmailInbox
             activeTab={activeTab}
             onEmailClick={handleEmailClick}
-            refreshKey={refreshKey}
           />
         </div>
 
